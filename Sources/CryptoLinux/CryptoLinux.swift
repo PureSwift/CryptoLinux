181dd6d63d412fdba455a2f8298dcb2495201513
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-05-06 13:16:22 -0700

    Working on `CryptoLinux`

--- Sources/CryptoLinux/CryptoLinux.swift
+++ Sources/CryptoLinux/CryptoLinux.swift
@@ -1,6 +1,40 @@
-public struct CryptoLinux {
-    public private(set) var text = "Hello, World!"
+//
+//  CryptoLinux.swift
+//
+//
+//  Created by Alsey Coleman Miller on 5/6/22.
+//
 
-    public init() {
+import SystemPackage
+import Socket
+
+/// Manages a socket to the [Linux Kernel Crypto API](https://www.kernel.org/doc/html/v5.10/crypto/userspace-if.html#)
+///
+/// The Linux kernel crypto API is accessible from user space. Currently, the following ciphers are accessible:
+///
+/// - Message digest including keyed message digest (HMAC, CMAC)
+/// - Symmetric ciphers
+/// - AEAD ciphers
+/// - Random Number Generators
+public struct CryptoLinux {
+    
+    // MARK: - Properties
+    
+    public static var path: FilePath { "/proc/crypto" }
+    
+    // MARK: - Initializaton
+    
+    public init() throws {
+        let fileDescriptor = try FileDescriptor.open(Self.path, .readOnly)
+        try fileDescriptor.closeAfter {
+        }
     }
+    
+    internal init(_ driverList: String) {
+        
+    }
+    
+    // MARK: - Methods
+    
+    
 }

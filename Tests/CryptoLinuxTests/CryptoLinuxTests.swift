6c7644aa357cd812298552a51ccf3ff95354f317
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-05-06 15:50:01 -0700

    Updated unit tests

--- Tests/CryptoLinuxTests/CryptoLinuxTests.swift
+++ Tests/CryptoLinuxTests/CryptoLinuxTests.swift
@@ -6,13 +6,27 @@ final class CryptoLinuxTests: XCTestCase {
     #if os(Linux)
     func testLoadDrivers() throws {
         let crypto = try CryptoLinux()
-        XCTAssert(crypto.ciphers.isEmpty == false)
+        XCTAssert(crypto.isEmpty == false)
     }
     #endif
     
-    func testDriverParsing() throws {
-        let crypto = CryptoLinux(genericList)
-        XCTAssert(crypto.ciphers.isEmpty == false)
+    func testCipherParsing() throws {
+        let crypto = try CryptoLinux(genericList, log: { print("Decoder:", $0) })
+        XCTAssert(crypto.isEmpty == false)
+        XCTAssertEqual(crypto.first?.name, "__ecb(aes)")
+        XCTAssertEqual(crypto.first?.driver, "cryptd(__ecb-aes-ce)")
+        XCTAssertEqual(crypto.first?.module, "cryptd")
+        XCTAssertEqual(crypto.first?.priority, 350)
+        XCTAssertEqual(crypto.first?.referenceCount, 2)
+        XCTAssertEqual(crypto.first?.selfTest, "passed")
+        XCTAssertEqual(crypto.first?.isInternal, true)
+        XCTAssertEqual(crypto.first?.async, true)
+        XCTAssertEqual(crypto.first?.blockSize, 16)
+        XCTAssertEqual(crypto.first?.minKeysize, 16)
+        XCTAssertEqual(crypto.first?.maxKeysize, 32)
+        XCTAssertEqual(crypto.first?.ivSize, 0)
+        XCTAssertEqual(crypto.first?.chunkSize, 16)
+        XCTAssertEqual(crypto.first?.walkSize, 16)
     }
 }
 

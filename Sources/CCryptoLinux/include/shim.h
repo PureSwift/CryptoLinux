42df6421aa8cd38e64d1378d15b412fe14b266f5
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-05-07 07:56:29 -0700

    Fixed C module

--- Sources/CCryptoLinux/include/shim.h
+++ Sources/CCryptoLinux/include/shim.h
@@ -1,14 +1,7 @@
 #ifdef __linux__
-#include <sys/epoll.h>
-#include <sys/eventfd.h>
-#include <sys/stat.h>
 #include <sys/socket.h>
-#include <sys/sysinfo.h>
-#include <sys/timerfd.h>
 #include <sys/types.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <pthread.h>
-#include <sched.h>
-#include <unistd.h>
+#include <linux/types.h>        /* for __u* and __s* typedefs */
+#include <linux/socket.h>        /* for "struct sockaddr" et al    */
+#include <linux/if_alg.h>
 #endif
